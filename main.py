1c215934b8cd8f885130d4358274a9400e05fa72
ragucwa <ra.gucwa@gmail.com>
2024-11-06 13:48:48 +0100

    First version

---
+++ main.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, jsonify
+import boto3
+from botocore.exceptions import NoCredentialsError, ClientError
+import polish_meds
+
+
+url = "https://rejestry.ezdrowie.gov.pl/api/rpl/medicinal-products/public-pl-report/5.0.0/overall.xml"
+file_path = "./downloaded_files/meds_list.xml"
+
+app = Flask(__name__)
+
+textract_client = boto3.client("textract")
+
+polish_meds.download_file(url, file_path)
+list_of_meds = polish_meds.get_meds(file_path)
+
+
+@app.route("/upload/", methods=["POST"])
+async def upload_image():
+    if "file" not in request.files:
+        return jsonify({"error": "No file part"}), 400
+
+    file = request.files["file"]
+
+    if file.filename == "":
+        return jsonify({"error": "No selected file"}), 400
+
+    try:
+        response = textract_client.detect_document_text(Document={"Bytes": file.read()})
+
+        extracted_text = []
+        for item in response["Blocks"]:
+            if item["BlockType"] == "LINE":
+                extracted_text.append(item["Text"])
+
+        texttract_results = set(word.lower() for word in extracted_text)
+
+        matched_med = polish_meds.match_meds(texttract_results, list_of_meds)
+
+        return jsonify({"matched_meds": matched_med})
+
+    except NoCredentialsError:
+        return jsonify({"error": "AWS credentials not availabke"}), 403
+    except ClientError as e:
+        return jsonify({"error": e.response["Error"]["Message"]}), 500
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ polish_meds.py
@@ -0,0 +1,33 @@
+import requests
+import pandas as pd
+
+
+def download_file(url: str, file_location: str):
+    response = requests.get(url, stream=True)
+
+    if response.status_code == 200:
+        with open(file_location, "wb") as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
+        print(f"File downloaded successfully and saved to {file_location}")
+    else:
+        print(f"Failed to download the file with {response.status_code}")
+
+
+def get_meds(file_location: str):
+    full_meds_table = pd.read_xml(file_location)
+    meds_names_table = full_meds_table["nazwaProduktu"]
+
+    return meds_names_table
+
+
+def match_meds(text_for_search: set, list_of_meds: pd.Series):
+    matched_med = ""
+
+    for medicine in list_of_meds:
+        medicine_words = medicine.lower().split()
+
+        if all(word in text_for_search for word in medicine_words):
+            matched_med = medicine
+
+    return matched_med
